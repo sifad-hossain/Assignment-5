09bbbc50669f84da7ce1bb65b12e5537c1c5ea5c
sifad-hossain <niaz.sifad@gmail.com>
2024-02-20 08:20:53 +0600

    jasascript first step done

--- script-js/index.js
+++ script-js/index.js
@@ -1,3 +1,32 @@
+// eita hoilo first rule:
+
+// function buyTicket() {
+//     //Step-1: Hide the Home Screen. To hide the screen  add the class hiden to the home section
+//     const busSection = document.getElementById('bus-container');
+//     // console.log(busSection.classList);
+//     busSection.classList.add('hidden');
+//     //Step-2:Show the Bus Paribhon
+//     const busTicketSale = document.getElementById('ticket-sale');
+//     // console.log(busTicketSale.classList);
+//     busTicketSale.classList.remove('hidden');
+// }
+
+
+
+// eita hoilo second rule:
+
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+
 function buyTicket() {
-    console.log('inshallah i will buy a new ticket');
+    hideElementById('bus-container');
+    showElementById('ticket-sale');
 }
