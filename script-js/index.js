e26a0b5cdad844e6900321135f4de5a5438335f3
sifad-hossain <niaz.sifad@gmail.com>
2024-02-22 08:56:20 +0600

    javascript strat

--- script-js/index.js
+++ script-js/index.js
@@ -15,18 +15,153 @@
 
 // eita hoilo second rule:
 
-function hideElementById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.add('hidden');
+// function hideElementById(elementId) {
+//     const element = document.getElementById(elementId);
+//     element.classList.add('hidden');
+// }
+
+// function showElementById(elementId) {
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('hidden');
+// }
+
+
+
+// function buyTicket() {
+//     hideElementById('bus-container');
+//     showElementById('ticket-sale');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function ticketSale(name) {
+//     console.log(name);
+//     // const firstTicket = document.getElementById('A1');
+
+// //    firstTicket.innerText = '';
+// //    setBackgrounColorById('A1');
+// //    setBackgrounColorById('A2');
+// //    setBackgrounColorById('A3');
+// //    setBackgrounColorById('A4');
+// }
+
+
+// function setBackgrounColorById(elementId) {
+//     const element = document.getElementById(elementId);
+//     element.classList.add('bg-[#1dd100]')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const allBtn = document.getElementsByClassName('add-btn');
+// // console.log(allBtn);
+
+// let count = 40;
+// for (const btn of allBtn) {
+//     btn.addEventListener('click', function () {
+//         count = count - 1;
+//         document.getElementById('ticket-btn').innerText = count;
+//     })
+// }
+
+const allBtn = document.getElementsByClassName('add-btn')
+let count = 8;
+
+for (const btn of allBtn) {
+    btn.addEventListener('click', function (e) {
+        count = count - 1;
+
+
+
+        const selectedContainer = document.getElementById('selected-place-container');
+
+        const price = 550;
+        const name = 'Economoy';
+        const btnName = document.getElementsByClassName('btn-name');
+       
+
+        const li = document.createElement('li');
+
+        const p = document.createElement('p');
+        p.innerText = btnName;
+        const p2 = document.createElement('p2');
+        p2.innerText = name;
+
+        const p3 = document.createElement('p3');
+        p3.innerText = price;
+
+
+
+        li.appendChild(p);
+        li.appendChild(p2);
+        li.appendChild(p3);
+        selectedContainer.appendChild(li);
+
+        const totalCost = document.getElementById('total-cost').innerText;
+
+        const convertedTotalCost = parseInt(totalCost);
+        const sum = convertedTotalCost + parseInt(price);
+        // console.log(convertedTotalCost);
+        // document.getElementById('total-cost').innerText = sum;
+        const grandTotal = document.getElementById('grand-total').innerText;
+        const convertedGrandTotal = parseInt(grandTotal);
+
+        const sum2 = convertedGrandTotal + parseInt(price);
+
+
+        setInnerText('total-cost', sum);
+        setInnerText('grand-total', sum2);        
+       setInnerText('ticket-btn', count);
+    })
 }
 
-function showElementById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.remove('hidden');
-}
-
-
-function buyTicket() {
-    hideElementById('bus-container');
-    showElementById('ticket-sale');
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
 }
